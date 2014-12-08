0ec82807e676f1fa964f54813e618ead13795e75
Valerio.Gheri <valerio.gheri@gmail.com>
2014-12-08 23:03:06 +0100

    Initial commit

---
+++ src/HelloMVC/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using HelloMVC.Models;
+using Microsoft.AspNet.Mvc;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            User user = new User()
+            {
+                Name = "My name",
+                Address = "My address"
+            };
+            return View(user);
+        }
+    }
+}
---
+++ src/HelloMVC/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace HelloMVC.Models
+{
+    public class User
+    {
+        [Required]
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Address { get; set; }
+    }
+}
